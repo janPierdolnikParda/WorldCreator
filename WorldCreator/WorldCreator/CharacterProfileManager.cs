b82a7fc8e79fa1926fdec0fb70112880e32005ff
Dominik <minikra@o2.pl>
2012-01-19 22:07:47 +0100

    profile.

--- WorldCreator/WorldCreator/CharacterProfileManager.cs
+++ WorldCreator/WorldCreator/CharacterProfileManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Mogre;
+using System.IO;
+using System.Xml;
 
 namespace WorldCreator
 {
@@ -17,29 +19,67 @@ namespace WorldCreator
 		{
 			C = new Dictionary<String, CharacterProfile>();
             E = new Dictionary<String, CharacterProfile>();
-			character = new CharacterProfile();
-			character.BodyMass = 70;
-			character.BodyScaleFactor = new Vector3(1.5f, 1, 1.5f);
-			character.HeadOffset = new Vector3(0, 0.8f, 0);
-			character.MeshName = "Man.mesh";
-			character.WalkSpeed = 1.85f;
-			character.PictureMaterial = "AdamMaterial";
-			character.DisplayNameOffset = new Vector3(0, 1, 0);
-			character.DisplayName = "Andrzej";
-            character.ProfileName = "cAndrzej";
-			C.Add("cAndrzej", character);
+            if (System.IO.File.Exists("Media\\Profiles\\Characters.xml"))
+            {
+                XmlDocument File = new XmlDocument();
+                File.Load("Media\\Profiles\\Characters.xml");
 
-            CharacterProfile enemy0 = new CharacterProfile();
-            enemy0.BodyMass = 70;
-            enemy0.BodyScaleFactor = new Vector3(1.5f, 1, 1.5f);
-            enemy0.HeadOffset = new Vector3(0, 0.8f, 0);
-            enemy0.MeshName = "Man.mesh";
-            enemy0.WalkSpeed = 1.85f;
-            enemy0.PictureMaterial = "AdamMaterial";
-            enemy0.DisplayNameOffset = new Vector3(0, 1, 0);
-            enemy0.ProfileName = "eAdam";
-            enemy0.DisplayName = "ADAM!";
-            E.Add("eAdam", enemy0);
+                XmlElement root = File.DocumentElement;
+                XmlNodeList Items = root.SelectNodes("//npcs//npc");
+
+                foreach (XmlNode item in Items)
+                {
+
+                    CharacterProfile Kriper = new CharacterProfile();
+                    Kriper.DisplayName = item["DisplayName"].InnerText;
+                    Kriper.MeshName = item["MeshName"].InnerText;
+                    Kriper.BodyMass = int.Parse(item["BodyMass"].InnerText);
+                    Kriper.WalkSpeed = float.Parse(item["WalkSpeed"].InnerText);
+                    Kriper.DisplayNameOffset = Vector3.ZERO;
+                    Kriper.DisplayNameOffset.x = float.Parse(item["DisplayNameOffset_x"].InnerText);
+                    Kriper.DisplayNameOffset.y = float.Parse(item["DisplayNameOffset_y"].InnerText);
+                    Kriper.DisplayNameOffset.z = float.Parse(item["DisplayNameOffset_z"].InnerText);
+                    Kriper.HeadOffset = Vector3.ZERO;
+                    Kriper.HeadOffset.x = float.Parse(item["HeadOffset_x"].InnerText);
+                    Kriper.HeadOffset.y = float.Parse(item["HeadOffset_y"].InnerText);
+                    Kriper.HeadOffset.z = float.Parse(item["HeadOffset_z"].InnerText);
+                    Kriper.ProfileName = item["ProfileName"].InnerText;
+
+                    C.Add(Kriper.ProfileName, Kriper);
+                }
+
+            }
+
+            if (System.IO.File.Exists("Media\\Profiles\\Enemies.xml"))
+            {
+                XmlDocument File = new XmlDocument();
+                File.Load("Media\\Profiles\\Enemies.xml");
+
+                XmlElement root = File.DocumentElement;
+                XmlNodeList Items = root.SelectNodes("//enemies//enemy");
+
+                foreach (XmlNode item in Items)
+                {
+
+                    CharacterProfile Kriper = new CharacterProfile();
+                    Kriper.DisplayName = item["DisplayName"].InnerText;
+                    Kriper.MeshName = item["MeshName"].InnerText;
+                    Kriper.BodyMass = int.Parse(item["BodyMass"].InnerText);
+                    Kriper.WalkSpeed = float.Parse(item["WalkSpeed"].InnerText);
+                    Kriper.DisplayNameOffset = Vector3.ZERO;
+                    Kriper.DisplayNameOffset.x = float.Parse(item["DisplayNameOffset_x"].InnerText);
+                    Kriper.DisplayNameOffset.y = float.Parse(item["DisplayNameOffset_y"].InnerText);
+                    Kriper.DisplayNameOffset.z = float.Parse(item["DisplayNameOffset_z"].InnerText);
+                    Kriper.HeadOffset = Vector3.ZERO;
+                    Kriper.HeadOffset.x = float.Parse(item["HeadOffset_x"].InnerText);
+                    Kriper.HeadOffset.y = float.Parse(item["HeadOffset_y"].InnerText);
+                    Kriper.HeadOffset.z = float.Parse(item["HeadOffset_z"].InnerText);
+                    Kriper.ProfileName = item["ProfileName"].InnerText;
+
+                    E.Add(item["ProfileName"].InnerText, Kriper);
+                }
+
+            }
 		}
     }
 }
